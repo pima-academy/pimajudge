203a4023ebdb391352177ac5811eb1ea9b858568
Naot <toanvv.ti.1922@gmail.com>
2025-12-20 14:56:06 +0700

    Add notification when run result

--- src/pimajudge/week1/jury.py
+++ src/pimajudge/week1/jury.py
@@ -5,4 +5,5 @@ answers: defaultdict[str, Optional[Union[dict, Callable]]] = defaultdict(lambda:
 
 
 def result(part: str):
+    print("Hiện tại trình chấm tự động chưa được công bố, hãy chờ thông báo từ đội ngũ giảng dạy.")
     pass
